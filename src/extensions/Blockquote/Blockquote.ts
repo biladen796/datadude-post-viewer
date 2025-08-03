7effe6230928f028ac214ef98aaf61a8382fdebe
VietNe <viet.nguyen@codelink.io>
2025-08-03 22:08:09 +0700

    feat: enhance Blockquote extension with custom view and avatar support

--- src/extensions/Blockquote/Blockquote.ts
+++ src/extensions/Blockquote/Blockquote.ts
@@ -1,31 +1,44 @@
-import type { BlockquoteOptions as TiptapBlockquoteOptions } from '@tiptap/extension-blockquote';
-import { Blockquote as TiptapBlockquote } from '@tiptap/extension-blockquote';
+import type { BlockquoteOptions as TiptapBlockquoteOptions } from "@tiptap/extension-blockquote";
+import { Blockquote as TiptapBlockquote } from "@tiptap/extension-blockquote";
 
-import { ActionButton } from '@/components';
-import type { GeneralOptions } from '@/types';
+import { ActionButton } from "@/components";
+import type { GeneralOptions } from "@/types";
+import { ReactNodeViewRenderer } from "@tiptap/react";
+import { BlockquoteView } from "@/extensions/Blockquote/components/BlockquoteView";
 
-export interface BlockquoteOptions
-  extends TiptapBlockquoteOptions,
-  GeneralOptions<BlockquoteOptions> {}
+export interface BlockquoteOptions extends TiptapBlockquoteOptions, GeneralOptions<BlockquoteOptions> {}
 
 export const Blockquote = /* @__PURE__ */ TiptapBlockquote.extend<BlockquoteOptions>({
   addOptions() {
     return {
       ...this.parent?.(),
       HTMLAttributes: {
-        class: 'blockquote',
+        class: "blockquote",
       },
       button: ({ editor, t }: any) => ({
         component: ActionButton,
         componentProps: {
           action: () => editor.commands.toggleBlockquote(),
-          isActive: () => editor.isActive('blockquote') || false,
+          isActive: () => editor.isActive("blockquote") || false,
           disabled: !editor.can().toggleBlockquote(),
-          icon: 'TextQuote',
-          shortcutKeys: ['shift', 'mod', 'B'],
-          tooltip: t('editor.blockquote.tooltip'),
+          icon: "TextQuote",
+          shortcutKeys: ["shift", "mod", "B"],
+          tooltip: t("editor.blockquote.tooltip"),
         },
       }),
     };
+  },
+  addAttributes() {
+    return {
+      quoteContent: {
+        default: "",
+        parseHTML: (element) => {
+          return element.textContent || "";
+        },
+      },
+    };
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer(BlockquoteView);
   },
 });
---
+++ src/extensions/Blockquote/components/BlockquoteView.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { NodeViewWrapper } from "@tiptap/react";
+import AvatarQuote from "./avatar.png";
+import QuoteIcon from "./quote.png";
+
+interface BlockquoteViewProps {
+  children: React.ReactNode;
+}
+
+export const BlockquoteView: React.FC<BlockquoteViewProps> = (props: any) => {
+  return (
+    <NodeViewWrapper className="blockquote">
+      <div className="blockquote-container">
+        <div className="blockquote-header">
+          <div className="richtext-flex richtext-justify-center richtext-items-center blockquote-icon-container">
+            <img src={QuoteIcon} alt="Quote" className="blockquote-icon" />
+          </div>
+          <div className="richtext-flex richtext-justify-center richtext-flex-1">
+            <img src={AvatarQuote} alt="Avatar" className="blockquote-avatar" />
+          </div>
+        </div>
+        <div className="richtext-italic">&ldquo; {props?.node?.attrs?.quoteContent || ""} &rdquo;</div>
+      </div>
+    </NodeViewWrapper>
+  );
+};
