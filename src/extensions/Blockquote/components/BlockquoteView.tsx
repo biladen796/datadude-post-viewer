0fc9d98d4c1a3db5a345287628ce52b4002c559e
VietNe <viet.nguyen@codelink.io>
2025-08-30 23:04:20 +0700

    refactor: clean up BlockquoteView by removing unused quote icon and commenting out border in editor.scss

--- src/extensions/Blockquote/components/BlockquoteView.tsx
+++ src/extensions/Blockquote/components/BlockquoteView.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { NodeViewWrapper } from "@tiptap/react";
+import React from "react";
 import AvatarQuote from "./avatar.png";
-import QuoteIcon from "./quote.png";
 
 interface BlockquoteViewProps {
   children: React.ReactNode;
@@ -12,9 +11,6 @@ export const BlockquoteView: React.FC<BlockquoteViewProps> = (props: any) => {
     <NodeViewWrapper className="blockquote">
       <div className="blockquote-container">
         <div className="blockquote-header">
-          <div className="richtext-flex richtext-justify-center richtext-items-center blockquote-icon-container">
-            <img src={QuoteIcon} alt="Quote" className="blockquote-icon" />
-          </div>
           <div className="richtext-flex richtext-justify-center richtext-flex-1">
             <img src={AvatarQuote} alt="Avatar" className="blockquote-avatar" />
           </div>
